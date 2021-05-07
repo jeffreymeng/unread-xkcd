da04805023a8c920f1d8f1a291e3b16f4d8da00a
Jeffrey Meng <jeffkmeng@gmail.com>
2021-05-06 23:10:45 -0700

    add webpack

---
+++ src/contentScript.js
@@ -0,0 +1,82 @@
+(async () => {
+
+  const {
+    addId,
+    hasId,
+    getReadComics,
+    addReadComic,
+    parseUrl
+  } = await import(chrome.runtime.getURL('modules/util.js'));
+
+  /**
+   *
+   * @returns {Promise<void>}
+   */
+  const markPageAsVisited = () => {
+    const id = parseUrl(window.location.href);
+    if (isNaN(id)) return;
+    return addReadComic(id);
+  };
+  markPageAsVisited();
+
+  /**
+   *
+   * @param min {number}
+   * @param max {number}
+   * @param visited {Set<number>}
+   * @returns {number}
+   */
+  const getRandomComic = (min, max, visited) => {
+    // generate an array with all unread comics
+    const unread = Array(max - min + 1)
+      .fill(0)
+      .map((v, i) => i + min)
+      .filter((v) => !hasId(visited, v));
+    // pick a random comic
+    // | 0 truncates
+    return unread[(unread.length * Math.random()) | 0];
+  };
+
+  /**
+   *
+   * @param text {string} - the name of the button
+   * @returns {HTMLAnchorElement[]}
+   */
+  const insertButtons = (text) => {
+    const links = [];
+    for (const nav of document.querySelectorAll(".comicNav")) {
+      // each nav needs an anchor with the right text
+      const a = document.createElement("a");
+      a.innerText = text;
+      links.push(a);
+      // the anchor needs to be wrapped in an li
+      const li = document.createElement("li");
+      li.appendChild(a);
+      // stick it into the dom before the next button
+      const next = nav.querySelector("li:nth-child(4)");
+      nav.insertBefore(li, next);
+    }
+    return links;
+  };
+
+  /**
+   * @returns {number}
+   */
+  const getLatestComicId = async () => {
+    const response = await fetch("https://xkcd.com/info.0.json");
+    const data = await response.json();
+    return data.num;
+  };
+
+  const links = insertButtons("Random Unread");
+  const id = getRandomComic(1, await getLatestComicId(), await getReadComics());
+  for (const link of links) {
+    if (id === undefined) {
+      link.href = "";
+      link.innerText = "No Unread Comics";
+      continue;
+    }
+    link.href = `/${id}/`;
+  }
+
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,47 @@
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  entry: {
+    contentScript: "./src/content-script.ts",
+    popup: "./src/popup/index.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
-    new CopyPlugin({
-      patterns: [{ from: "src", to: "dist" }],
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/manifest.json",
+        },
+        {
+          from: "src/icons",
+          to: "icons",
+        },
+        {
+          from: "src/popup",
+          to: "./",
+          filter: (path) => {
+            return /\.(html|css)$/.test(path);
+          },
+        },
+      ],
     }),
   ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
